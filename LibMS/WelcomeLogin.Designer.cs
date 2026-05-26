a7d6ee3d0c781ba167ced91ad5acd035a279ef7a
Hannah Arroyo <iyah08302005@gmail.com>
2026-05-27 07:35:23 +0800

    Add files via upload

---
+++ LibMS/WelcomeLogin.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace LibMS
+{
+    partial class WelcomeLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelleft1 = new Panel();
+            panellogin = new Panel();
+            visible1 = new Button();
+            btnAdm1 = new Button();
+            btnStud1 = new Button();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            pictureBox6 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            label4 = new Label();
+            pictureBox4 = new PictureBox();
+            label3 = new Label();
+            btnLogin1 = new Button();
+            lblFgPass = new Label();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            txtPass1 = new TextBox();
+            txtEmail1 = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            hide1 = new Button();
+            panellogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panelleft1
+            // 
+            panelleft1.BackColor = Color.RoyalBlue;
+            panelleft1.BackgroundImage = Properties.Resources.Screenshot_2026_05_20_153608;
+            panelleft1.BackgroundImageLayout = ImageLayout.Stretch;
+            panelleft1.BorderStyle = BorderStyle.FixedSingle;
+            panelleft1.Location = new Point(12, 12);
+            panelleft1.Name = "panelleft1";
+            panelleft1.Size = new Size(411, 562);
+            panelleft1.TabIndex = 0;
+            // 
+            // panellogin
+            // 
+            panellogin.BackColor = Color.White;
+            panellogin.BorderStyle = BorderStyle.FixedSingle;
+            panellogin.Controls.Add(btnAdm1);
+            panellogin.Controls.Add(btnStud1);
+            panellogin.Controls.Add(label8);
+            panellogin.Controls.Add(label7);
+            panellogin.Controls.Add(label6);
+            panellogin.Controls.Add(label5);
+            panellogin.Controls.Add(pictureBox6);
+            panellogin.Controls.Add(pictureBox5);
+            panellogin.Controls.Add(label4);
+            panellogin.Controls.Add(pictureBox4);
+            panellogin.Controls.Add(label3);
+            panellogin.Controls.Add(btnLogin1);
+            panellogin.Controls.Add(lblFgPass);
+            panellogin.Controls.Add(pictureBox3);
+            panellogin.Controls.Add(pictureBox2);
+            panellogin.Controls.Add(txtPass1);
+            panellogin.Controls.Add(txtEmail1);
+            panellogin.Controls.Add(label2);
+            panellogin.Controls.Add(label1);
+            panellogin.Controls.Add(pictureBox1);
+            panellogin.Controls.Add(visible1);
+            panellogin.Controls.Add(hide1);
+            panellogin.Location = new Point(429, 12);
+            panellogin.Name = "panellogin";
+            panellogin.Size = new Size(565, 562);
+            panellogin.TabIndex = 1;
+            // 
+            // visible1
+            // 
+            visible1.FlatAppearance.BorderSize = 0;
+            visible1.FlatStyle = FlatStyle.Flat;
+            visible1.Image = Properties.Resources.visible__1_1;
+            visible1.Location = new Point(428, 279);
+            visible1.Name = "visible1";
+            visible1.Size = new Size(27, 27);
+            visible1.TabIndex = 0;
+            visible1.UseVisualStyleBackColor = true;
+            visible1.Click += visible1_Click;
+            // 
+            // btnAdm1
+            // 
+            btnAdm1.FlatStyle = FlatStyle.Flat;
+            btnAdm1.Location = new Point(288, 187);
+            btnAdm1.Name = "btnAdm1";
+            btnAdm1.Size = new Size(124, 29);
+            btnAdm1.TabIndex = 22;
+            btnAdm1.Text = "ADMIN";
+            btnAdm1.UseVisualStyleBackColor = true;
+            btnAdm1.Click += btnAdm1_Click;
+            // 
+            // btnStud1
+            // 
+            btnStud1.FlatStyle = FlatStyle.Flat;
+            btnStud1.Location = new Point(168, 187);
+            btnStud1.Name = "btnStud1";
+            btnStud1.Size = new Size(124, 29);
+            btnStud1.TabIndex = 21;
+            btnStud1.Text = "STUDENT";
+            btnStud1.UseVisualStyleBackColor = true;
+            btnStud1.Click += btnStud_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 8F);
+            label8.Location = new Point(421, 478);
+            label8.Name = "label8";
+            label8.Size = new Size(85, 19);
+            label8.TabIndex = 20;
+            label8.Text = "Notifications";
+            label8.Click += label8_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 8F);
+            label7.Location = new Point(421, 463);
+            label7.Name = "label7";
+            label7.Size = new Size(97, 19);
+            label7.TabIndex = 19;
+            label7.Text = "Get Important";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 8F);
+            label6.Location = new Point(248, 478);
+            label6.Name = "label6";
+            label6.Size = new Size(46, 19);
+            label6.TabIndex = 18;
+            label6.Text = "Books";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 8F);
+            label5.Location = new Point(248, 463);
+            label5.Name = "label5";
+            label5.Size = new Size(122, 19);
+            label5.TabIndex = 17;
+            label5.Text = "Manage Borrowed";
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = Properties.Resources.b9c273f4_68a1_4133_8051_5380e0291e5a;
+            pictureBox6.Location = new Point(376, 457);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(51, 40);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 16;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources._14d61453_b879_4b0f_a3ad_985fa975cf4f;
+            pictureBox5.Location = new Point(197, 463);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(45, 34);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 15;
+            pictureBox5.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 8F);
+            label4.Location = new Point(105, 480);
+            label4.Name = "label4";
+            label4.Size = new Size(58, 19);
+            label4.TabIndex = 14;
+            label4.Text = "anytime";
+            label4.Click += label4_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources._72c1ae10_67a3_4ee9_b30e_06aeced03849;
+            pictureBox4.Location = new Point(55, 463);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(44, 36);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 13;
+            pictureBox4.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(105, 463);
+            label3.Name = "label3";
+            label3.Size = new Size(86, 17);
+            label3.TabIndex = 12;
+            label3.Text = "Access Books";
+            // 
+            // btnLogin1
+            // 
+            btnLogin1.BackColor = Color.Gold;
+            btnLogin1.FlatStyle = FlatStyle.Flat;
+            btnLogin1.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnLogin1.Location = new Point(121, 370);
+            btnLogin1.Name = "btnLogin1";
+            btnLogin1.Size = new Size(334, 31);
+            btnLogin1.TabIndex = 11;
+            btnLogin1.Text = "LOG IN";
+            btnLogin1.UseVisualStyleBackColor = false;
+            btnLogin1.Click += btnLogin1_Click;
+            // 
+            // lblFgPass
+            // 
+            lblFgPass.AutoSize = true;
+            lblFgPass.Cursor = Cursors.Hand;
+            lblFgPass.ForeColor = Color.Goldenrod;
+            lblFgPass.Location = new Point(330, 328);
+            lblFgPass.Name = "lblFgPass";
+            lblFgPass.Size = new Size(125, 20);
+            lblFgPass.TabIndex = 9;
+            lblFgPass.Text = "Forgot Password?";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.password_1801;
+            pictureBox3.Location = new Point(121, 279);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(25, 25);
+            pictureBox3.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox3.TabIndex = 8;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.pngtree_email_envelope_sign_vector_icon_jpg_png_image_21364576;
+            pictureBox2.Location = new Point(121, 232);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(28, 27);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 7;
+            pictureBox2.TabStop = false;
+            // 
+            // txtPass1
+            // 
+            txtPass1.Location = new Point(158, 279);
+            txtPass1.Name = "txtPass1";
+            txtPass1.PasswordChar = '*';
+            txtPass1.Size = new Size(269, 27);
+            txtPass1.TabIndex = 6;
+            txtPass1.Text = "Password";
+            // 
+            // txtEmail1
+            // 
+            txtEmail1.Location = new Point(158, 232);
+            txtEmail1.Name = "txtEmail1";
+            txtEmail1.Size = new Size(297, 27);
+            txtEmail1.TabIndex = 5;
+            txtEmail1.Text = "Email Address";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(158, 139);
+            label2.Name = "label2";
+            label2.Size = new Size(277, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Please enter your credentials to continue";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(186, 116);
+            label1.Name = "label1";
+            label1.Size = new Size(226, 23);
+            label1.TabIndex = 1;
+            label1.Text = "Log in to your account";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources._673884223_1306094024959872_5643997104342755968_n;
+            pictureBox1.Location = new Point(138, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(320, 170);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // hide1
+            // 
+            hide1.FlatAppearance.BorderSize = 0;
+            hide1.FlatStyle = FlatStyle.Flat;
+            hide1.Image = Properties.Resources.hide__1_;
+            hide1.Location = new Point(428, 279);
+            hide1.Name = "hide1";
+            hide1.Size = new Size(27, 27);
+            hide1.TabIndex = 0;
+            hide1.UseVisualStyleBackColor = true;
+            hide1.Visible = false;
+            hide1.Click += hide1_Click;
+            // 
+            // WelcomeLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.WhiteSmoke;
+            ClientSize = new Size(1006, 586);
+            Controls.Add(panellogin);
+            Controls.Add(panelleft1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "WelcomeLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Student Login";
+            panellogin.ResumeLayout(false);
+            panellogin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelleft1;
+        private Panel panellogin;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox txtPass1;
+        private TextBox txtEmail1;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private Label lblFgPass;
+        private Button btnLogin1;
+        private PictureBox pictureBox4;
+        private Label label3;
+        private PictureBox pictureBox5;
+        private Label label4;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private PictureBox pictureBox6;
+        private Button btnAdm1;
+        private Button btnStud1;
+        private Button visible1;
+        private Button hide1;
+    }
+}
